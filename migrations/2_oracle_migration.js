736ac6e2445825591b2ea1a4e4b6bb5fe1e59415
tang <tang@qq.comcom>
2022-06-05 23:08:14 +0800

    add migrations

--- migrations/2_oracle_migration.js
+++ migrations/2_oracle_migration.js
@@ -1,10 +1,14 @@
 const TemperatureOracle = artifacts.require("Oracle/TemperatureOracle");
 const numbers = artifacts.require("Oracle/util/numbers");
+const strings = artifacts.require("Oracle/util/strings");
 
 
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(numbers);
   deployer.link(numbers, [TemperatureOracle]);
+
+  deployer.deploy(strings);
+  deployer.link(strings, [TemperatureOracle]);
 
   deployer.deploy(TemperatureOracle, accounts[0], 3, "-100.00", "100.00");
 };
